e822451a85fde471f23902ae39baea374acd9d4e
Shashank Motruri <motrurishashank000@gmail.com>
2021-10-03 19:45:52 +0530

    fix : list files

--- server/utills/google.js
+++ server/utills/google.js
@@ -1,27 +1,23 @@
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const getListOfFiles = async (list, access_token) => {
-  const arr = [];
-  if (!list) {
-    return null;
-  }
-  for (let i = 0; i < list.length; i++) {
-    await fetch(`${list[i].childLink}`, {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + `${access_token}`,
-      },
-    })
-      .then(result => result.json())
-      .then(response => {
-        arr.push(response);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  return arr;
-};
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-module.exports = { getListOfFiles };
+const getListOfFiles = async (list,access_token) => {
+    for (let i = 0; i < list.length; i++) {   
+        await fetch(`${list[i].childLink}`, {
+            method: 'GET',
+            headers: {
+            'Authorization': 'Bearer ' + `${access_token}` },
+        })
+        .then((result) => {
+            return result.json()
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+}
+
+module.exports = {getListOfFiles};
