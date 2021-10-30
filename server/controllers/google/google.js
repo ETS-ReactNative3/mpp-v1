df723ae9570e9834e3e5ce441dfa8d7ea26a4bab
shashankmotruri <motrurishashank000@gmail.com>
2021-10-30 11:16:06 +0530

    Add mongoose changes

--- server/controllers/google/google.js
+++ server/controllers/google/google.js
@@ -1,16 +1,29 @@
-const { google } = require('googleapis');
+const {
+  google
+} = require('googleapis');
 const path = require('path');
 const fs = require('fs');
 const stream = require('stream');
 // var FormData = require('form-data');
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const { linkDriveDB } = require('../../service/user.js');
-const { responder } = require('../../utills/responseHandler.js');
-const { getListOfFiles, getValidTokens} = require('../../utills/google.js');
+  import('node-fetch').then(({
+    default: fetch
+  }) => fetch(...args));
+const {
+  linkDriveDB
+} = require('../../service/user.js');
+const {
+  responder
+} = require('../../utills/responseHandler.js');
+const {
+  getListOfFiles,
+  getValidTokens
+} = require('../../utills/google.js');
 
 const utils = require('./Oauthmodule');
-const {drive} = utils;
+const {
+  drive
+} = utils;
 const driveutils = require('./Drivmodule');
 
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -21,13 +34,13 @@ const TOKEN_PATH = 'token.json';
 const refreshToken = async (req, res, next) => {
   try {
     await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST', 
-      body: new URLSearchParams({
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        refresh_token: REFRESH_TOKEN,
-        grant_type: 'refresh_token',
-      })
+        method: 'POST',
+        body: new URLSearchParams({
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          refresh_token: REFRESH_TOKEN,
+          grant_type: 'refresh_token',
+        })
       })
       .then(response => response.json())
       .then(data => {
@@ -36,7 +49,7 @@ const refreshToken = async (req, res, next) => {
       .catch((error) => {
         return null;
       });
-    
+
   } catch (error) {
     return null;
   }
@@ -45,67 +58,69 @@ const refreshToken = async (req, res, next) => {
 const linkDrive = async (req, res, next) => {
   try {
     let url = utils.gEtURL();
-    res.status(200).send({ url: url });
+    res.status(200).send({
+      url: url
+    });
   } catch (error) {
-    res.status(400).send({ error: ['Error in authenticating OAuth', error] });
+    res.status(400).send({
+      error: ['Error in authenticating OAuth', error]
+    });
   }
 };
 
 const callBack = async (req, res, next) => {
-  const {code} = req.query;
+  const {
+    code
+  } = req.query;
   let user_email;
   let id_token;
-  if(code){
-   await utils.oAuth2Client.getToken(code, function(err,tokens){
-      if (err) {
-        console.error("Error in authenticating");
-        res.redirect('http://localhost:5000/storyline/new');
-      } else {
-        console.log("Successfully authenticated");
-        id_token = tokens.id_token;
-        utils.sToreToken(tokens);
-        utils.oAuth2Client.credentials = (tokens);
-        console.log("Id TOKEN");
-        console.log(id_token);
-      }
-    });
+  if (code) {
+    try {
+      let tokens_res = await utils.oAuth2Client.getToken(code)
+      utils.sToreToken(tokens_res.tokens);
+      utils.oAuth2Client.credentials = (tokens_res.tokens);
+      console.log(tokens_res.tokens)
+      id_token = tokens_res.tokens.id_token;
+    } catch (error) {
+      console.log(error);
+    }
+  
     console.log(id_token);
-    await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`)
+    console.log(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`);
+    user_email = await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`)
+      .then((response) => {
+        return response.json();
+      })
       .then((json) => {
-        console.log("EMAIL");
         console.log(json);
-        console.log(json.json());
-        console.log(json.response);
-        console.log("data");
-        console.log(json.data);
-        user_email = json.email;
+        return json.email;
       })
       .catch(error => {
         console.log(error);
-    });
+      });
+
+  } else {
+    return responder(res)(500)
   }
-    else {
-      return responder(res)(500)
-    }
-    console.log("Fetching id's");
-    let fids = await driveutils.iSfolderExist();
-    console.log(fids);
-    user_email = "shashank.m19@iiits.in";
-    console.log(user_email);
-    await linkDriveDB(user_email,fids);
-    res.redirect('http://localhost:5000/storyline/new');
+  console.log("Fetching id's");
+  let fids = await driveutils.iSfolderExist();
+  console.log(fids);
+  console.log("EMAIL");
+  console.log(user_email);
+  await linkDriveDB(user_email, fids);
+  res.redirect('http://localhost:5000/storyline/new');
 };
 
 const deleteFile = async (req, res, next) => {
   let tokens = require('../../../token.json');
   let newTokens = await getValidTokens(tokens);
   utils.oAuth2Client.credentials = newTokens;
-  
+
   await drive.files.delete({
-    auth: utils.oAuth2Client,
-    fileId: req.params.id,
+      auth: utils.oAuth2Client,
+      fileId: req.params.id,
     },
-    function(err, response) {
+    function (err, response) {
       if (err) {
         console.log(`The API returned an error: ${  err}`);
         return responder(res)(err, null);
@@ -119,18 +134,17 @@ const updateFile = async (req, res, next) => {
   let tokens = require('../../../token.json');
   let newTokens = await getValidTokens(tokens);
   utils.oAuth2Client.credentials = newTokens;
-  await drive.files.get(
-    {
-      auth: utils.oAuth2Client,
-      fileId: req.params.id,
-      media: media,
+  await drive.files.get({
+    auth: utils.oAuth2Client,
+    fileId: req.params.id,
+    media: media,
   }, (err, response) => {
-      if (err) {
-          console.log(err);
-          responder(400)(err, null);
-      }
-      console.log(response.data)
-      responder(res)(null, response.data);
+    if (err) {
+      console.log(err);
+      responder(400)(err, null);
+    }
+    console.log(response.data)
+    responder(res)(null, response.data);
   });
 };
 
@@ -139,14 +153,13 @@ const getFile = async (req, res, next) => {
   utils.oAuth2Client.credentials = tokens;
   let newTokens = await getValidTokens(tokens);
   await fetch(
-      `https://www.googleapis.com/drive/v2/files/${req.params.id}?alt=media&source=downloadUrl`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + `${newTokens.access_token}`,
+      `https://www.googleapis.com/drive/v2/files/${req.params.id}?alt=media&source=downloadUrl`, {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + `${newTokens.access_token}`,
+        },
       },
-    },
-  )
+    )
     .then((result) => result.json())
     .then((response) => {
       responder(res)(null, response);
@@ -161,17 +174,17 @@ const uploadFile = async (req, res, next, data) => {
   fs.writeFileSync(baseDir, data);
 
   try {
-      const fileMetadata = {
-        name: 'mpp.json',
-        parents: ["1urJh-QUxraU-VXBGI13lpbK9b81crcBP"],
-      };
-      const media = {
-        mimeType: 'application/json',
-        body: fs.createReadStream(baseDir),
-      };
-      console.log('Sending file...');
-      const resdata = await driveutils.sEndFile(fileMetadata, media);
-      responder(res)(null, resdata);
+    const fileMetadata = {
+      name: 'mpp.json',
+      parents: ["1urJh-QUxraU-VXBGI13lpbK9b81crcBP"],
+    };
+    const media = {
+      mimeType: 'application/json',
+      body: fs.createReadStream(baseDir),
+    };
+    console.log('Sending file...');
+    const resdata = await driveutils.sEndFile(fileMetadata, media);
+    responder(res)(null, resdata);
   } catch (error) {
     console.log(error);
     responder(res)(error, null);
@@ -186,14 +199,13 @@ const listFiles = async (req, res, next) => {
   const access_token = (newTokens.access_token) ? newTokens.access_token : null;
 
   await fetch(
-    'https://www.googleapis.com/drive/v2/files/1fkPN96QOmCvLNkR7Puw7vqkwhsstsGop/children',
-    {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + `${access_token}`,
+      'https://www.googleapis.com/drive/v2/files/1fkPN96QOmCvLNkR7Puw7vqkwhsstsGop/children', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + `${access_token}`,
+        },
       },
-    },
-  )
+    )
     .then((result) => result.json())
     .then((response) => {
       fileList = response;
