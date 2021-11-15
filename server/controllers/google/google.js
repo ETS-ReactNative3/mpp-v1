f3e700e2e1acb1cca25f79ab758ec354726c01ea
Rishabh Malik <workwithmalik97@gmail.com>
2021-11-15 17:47:44 +0530

    change in controllers of google

--- server/controllers/google/google.js
+++ server/controllers/google/google.js
@@ -3,89 +3,74 @@ const fs = require('fs');
 const stream = require('stream');
 
 const fetch = (...args) =>
-  import('node-fetch').then(({
-    default: fetch
-  }) => fetch(...args));
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const {
   linkDriveDB,
   updateTokens,
   getTokens,
   getRefreshToken,
-  getDriveDetails
+  getDriveDetails,
 } = require('../../service/user.js');
 
 const {
   getListOfFiles,
   getValidTokens,
-  getFileList
+  getFileList,
 } = require('../../utills/google.js');
-const {
-  getEmail,
-  getEmailWithId
-} = require('../../utills/utills.js');
-const {
-  validateAccess
-} = require('../../utills/manageTokens.js');
+const { getEmail, getEmailWithId } = require('../../utills/utills.js');
+const { validateAccess } = require('../../utills/manageTokens.js');
 
 const utils = require('./Oauthmodule');
-const {
-  drive
-} = utils;
+const { drive } = utils;
 const driveutils = require('./Drivmodule');
 
 const linkDrive = async (req, res, next) => {
   try {
-    let url = utils.getURL();
+    const url = utils.getURL();
     res.status(200).send({
-      url: url
+      url,
     });
   } catch (error) {
     res.status(400).send({
-      error: ['Error in authenticating OAuth', error]
+      error: ['Error in authenticating OAuth', error],
     });
   }
 };
 
 const callBack = async (req, res, next) => {
-  const {
-    code
-  } = req.query;
-
+  const { code } = req.query;
 
   if (code) {
-
     let user_email;
     try {
+      const currTokens = await utils.oAuth2Client.getToken(code);
 
-      let currTokens = await utils.oAuth2Client.getToken(code);
-
-      let tokens = currTokens.tokens;
+      const { tokens } = currTokens;
 
       user_email = await getEmailWithId(tokens.id_token);
 
       await utils.storeToken(user_email, tokens);
 
-      utils.oAuth2Client.credentials = (tokens);
-
+      utils.oAuth2Client.credentials = tokens;
     } catch (error) {
       return res.status(500).send({
-        msg: "Error In Authentication"
+        msg: 'Error In Authentication',
       });
     }
 
-    console.log("Successfully authenticated")
+    console.log('Successfully authenticated');
 
     if (!user_email) {
       return res.status(400).send({
-        msg: "Invalid id_token"
+        msg: 'Invalid id_token',
       });
     }
 
-    let googletokens = await getTokens(user_email);
+    const googletokens = await getTokens(user_email);
 
     console.log("Fetching id's");
-    console.log("Email = " + user_email);
-    let fids = await driveutils.isFolderExist(googletokens);
+    console.log(`Email = ${user_email}`);
+    const fids = await driveutils.isFolderExist(googletokens);
 
     if (fids) {
       await linkDriveDB(user_email, fids);
@@ -96,106 +81,115 @@ const callBack = async (req, res, next) => {
     res.redirect(process.env.REDIRECT_URL);
   } else {
     return res.status(500).send({
-      msg: 'Internal server error'
+      msg: 'Internal server error',
     });
   }
 };
 
 const deleteFile = async (req, res, next) => {
-  const {email,newTokens} = await validateAccess(req, res, next);
+  const { email, newTokens } = await validateAccess(req, res, next);
 
-  if(!email || !newTokens) {
-    return res.status(401).send({mg : "Failed Validation"});
+  if (!email || !newTokens) {
+    return res.status(401).send({ mg: 'Failed Validation' });
   }
   utils.oAuth2Client.credentials = newTokens;
-  drive.files.delete({
+  drive.files.delete(
+    {
       auth: utils.oAuth2Client,
       fileId: req.params.id,
     },
-    function (err, response) {
+    function(err, response) {
       if (err) {
-        console.log(`The API returned an error: ${  err}`);
+        console.log(`The API returned an error: ${err}`);
         return res.status(401).send({
-          error: err
+          error: err,
         });
       }
       return res.status(200).send({
-        msg: "Deleted file successfully",
-        response
+        msg: 'Deleted file successfully',
+        response,
       });
     },
   );
 };
 
 const updateFile = async (req, res, next) => {
-  const {email,newTokens} = await validateAccess(req, res, next);
+  const { email, newTokens } = await validateAccess(req, res, next);
 
-  if(!email || !newTokens) {
-    return res.status(401).send({mg : "Failed Validation"});
+  if (!email || !newTokens) {
+    return res.status(401).send({ mg: 'Failed Validation' });
   }
   utils.oAuth2Client.credentials = newTokens;
   const content = JSON.stringify(req.body);
   const buf = Buffer.from(content, 'binary');
   const buffer = Uint8Array.from(buf);
-  var bufferStream = new stream.PassThrough();
+  const bufferStream = new stream.PassThrough();
   bufferStream.end(buffer);
   const media = {
-      mimeType: 'application/json',
-      body: bufferStream,
+    mimeType: 'application/json',
+    body: bufferStream,
   };
-  let file_name = req.body.title ? `${req.body.title.replace(/\s/g, '_') }.json`: "story_line.json";
-  drive.files.update({
-    auth: utils.oAuth2Client,
-    fileId: req.params.id,
-    media: media,
-    resource : {name : file_name}
-  }, (err, response) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({
-        error: err
+  const file_name = req.body.title
+    ? `${req.body.title.replace(/\s/g, '_')}.json`
+    : 'story_line.json';
+  drive.files.update(
+    {
+      auth: utils.oAuth2Client,
+      fileId: req.params.id,
+      media,
+      resource: { name: file_name },
+    },
+    (err, response) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).send({
+          error: err,
+        });
+      }
+      return res.status(200).send({
+        msg: 'Updated file successfully',
+        response,
       });
-    }
-    return res.status(200).send({
-      msg: "Updated file successfully",
-      response
-    });
-  });
+    },
+  );
 };
 
 const getFile = async (req, res, next) => {
-  const {email,newTokens} = await validateAccess(req, res, next);
+  const { email, newTokens } = await validateAccess(req, res, next);
 
-  if(!email || !newTokens) {
-    return res.status(401).send({mg : "Failed Validation"});
+  if (!email || !newTokens) {
+    return res.status(401).send({ mg: 'Failed Validation' });
   }
 
   await fetch(
-      `https://www.googleapis.com/drive/v2/files/${req.params.id}?alt=media&source=downloadUrl`, {
-        method: 'GET',
-        headers: {
-          Authorization: 'Bearer ' + `${newTokens.access_token}`,
-        },
+    `https://www.googleapis.com/drive/v2/files/${
+      req.params.id
+    }?alt=media&source=downloadUrl`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + `${newTokens.access_token}`,
       },
+    },
+  )
+    .then(result => result.json())
+    .then(response =>
+      res.status(200).send({
+        response,
+      }),
     )
-    .then((result) => result.json())
-    .then((response) => {
-      return res.status(200).send({
-        response
-      });
-    })
-    .catch((err) => {
-      return res.status(401).send({
-        error: err
-      });
-    })
+    .catch(err =>
+      res.status(401).send({
+        error: err,
+      }),
+    );
 };
 
 const uploadFile = async (req, res, next, data) => {
-  const {email,newTokens} = await validateAccess(req, res, next);
+  const { email, newTokens } = await validateAccess(req, res, next);
 
-  if(!email || !newTokens) {
-    return res.status(401).send({mg : "Failed Validation"});
+  if (!email || !newTokens) {
+    return res.status(401).send({ mg: 'Failed Validation' });
   }
   const baseDir = path.join(__dirname, '../../storage/temp.json');
   fs.writeFileSync(baseDir, data);
@@ -203,17 +197,19 @@ const uploadFile = async (req, res, next, data) => {
   const driveIds = await getDriveDetails(email);
   if (!driveIds) {
     return res.status(401).send({
-      msg: "Drive Not Linked.Plese Link Drive..."
-    })
+      msg: 'Drive Not Linked.Plese Link Drive...',
+    });
   }
 
-  //console.log(driveIds.p_fid)
+  // console.log(driveIds.p_fid)
 
-  let file_name = req.body.title ? `${req.body.title.replace(/\s/g, '_') }.json`: "story_line.json";
-  console.log("Title : " + req.body.title);
+  const file_name = req.body.title
+    ? `${req.body.title.replace(/\s/g, '_')}.json`
+    : 'story_line.json';
+  console.log(`Title : ${req.body.title}`);
 
   console.log(file_name);
-  //console.log("file to be saved at id : " + driveIds.myp_fid);
+  // console.log("file to be saved at id : " + driveIds.myp_fid);
   try {
     const fileMetadata = {
       name: file_name,
@@ -225,33 +221,31 @@ const uploadFile = async (req, res, next, data) => {
     };
     console.log('Sending file...');
     return await driveutils.sendFile(res, fileMetadata, media, newTokens);
-
   } catch (error) {
     console.log(error);
     res.status(500).send({
-      msg: "Error Sending File",
-      error: error
+      msg: 'Error Sending File',
+      error,
     });
   }
 };
 
 const listFiles = async (req, res, next) => {
- 
-  const {email,newTokens} = await validateAccess(req, res, next);
+  const { email, newTokens } = await validateAccess(req, res, next);
 
-  if(!email || !newTokens) {
-    return res.status(401).send({mg : "Failed Validation"});
+  if (!email || !newTokens) {
+    return res.status(401).send({ mg: 'Failed Validation' });
   }
   let fileList = [];
 
-  console.log("Successfully upated tokens to DB...");
-  const access_token = (newTokens.access_token) ? newTokens.access_token : null;
+  console.log('Successfully upated tokens to DB...');
+  const access_token = newTokens.access_token ? newTokens.access_token : null;
 
   const driveIds = await getDriveDetails(email);
   if (!driveIds) {
     return res.status(401).send({
-      msg: "Drive Not Linked.Plese Link Drive..."
-    })
+      msg: 'Drive Not Linked.Plese Link Drive...',
+    });
   }
 
   fileList = await getFileList(access_token, driveIds);
@@ -268,5 +262,5 @@ module.exports = {
   listFiles,
   getFile,
   updateFile,
-  deleteFile
+  deleteFile,
 };
