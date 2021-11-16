8639749a55734621f11bb037656905081d1f2387
Rishabh Malik <workwithmalik97@gmail.com>
2021-11-16 22:44:15 +0530

    change in utils dashboard

--- app/utils/APIcalls/dashboard.js
+++ app/utils/APIcalls/dashboard.js
@@ -1,38 +1,38 @@
-
-export const GetDashboardInfo = (authToken) => {
-    return fetch('/api/dashboard/dashboardInfo',{
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-auth-token': authToken
-      }
+export const GetDashboardInfo = authToken => {
+  return fetch('/api/dashboard/dashboardInfo', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-auth-token': authToken,
+    },
   })
     .then(function(response) {
       return response.json();
     })
     .then(function(json) {
-        return json;
+      return json;
     })
     .catch(err => {
       console.log(err);
     });
-} 
+};
 
-export const linkDrive = (authToken) => {
-    return   fetch('/api/google/linkDrive', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-auth-token': authToken
-      }
+export const linkDrive = authToken => {
+  return fetch('/api/google/linkDrive', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-auth-token': authToken,
+    },
   })
     .then(function(response) {
       return response.json();
     })
     .then(function(res) {
-        window.open(`${res.url}`)
+      console.log(res);
+      window.open(`${res.url}`);
     })
     .catch(err => {
       console.log(err);
     });
-} 
+};
